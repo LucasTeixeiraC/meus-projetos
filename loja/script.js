535ce4cf21c042e215cc5debb7ace0b74cdde3f3
LucasTeixeiraC <uchih43@gmail.com>
2023-09-20 13:47:26 +0100

    new

--- loja/script.js
+++ loja/script.js
@@ -1,31 +1,28 @@
-let cartIcon = document.querySelector('#cart-icon')
-let cart= document.querySelector('.cart')
-let closeCart = document.querySelector('#close-cart')
+let cartIcon = document.querySelector("#cart-icon")
+let cart = document.querySelector(".cart")
+let closeCart = document.querySelector("#close-cart")
 
 cartIcon.onclick = () => {
-    cart.classList.add("active");
-};
-cartIcon.onclick = () => {
-    cart.classList.remove('active');
+    cart.classList.add ("active")
+}
+closeCart.onclick = () => {
+    cart.classList.remove ("active")
 }
 
 
-if(document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready);
-}else {
-    ready ();
+if (document.readyState == 'loading') {
+    document.addEventListener("DOMContentLoaded", ready);
+    ready();
 }
-
-function ready() {
-    var removeCartButtons = document.getElementsByClassName('cart-remove')
-    console.log(removeCartButtons)
-    for (var i = 0; i < removeCartButtons.length; i++) {
-        var button = removeCartButtons[i]
-        button.addEventListener ('click', removeCartItem)
+function ready () {
+    var reomveCartButtons = document.getElementsByClassName('cart-remove')
+    console.log(reomveCartButtons)
+    for (var i = 0; i < reomveCartButtons.length; i++) {
+        var button = reomveCartButtons[i]
+        button.addEventListener('click', removeCartItem)
     }
-
 }
-function removeCartItem (event) {
+function removeCartItem(event) {
     var buttonClicked = event.target
     buttonClicked.parentElement.remove()
 }
