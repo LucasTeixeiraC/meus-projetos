c4920127bfd4be83f0362207bd5b65203d239ec3
LucasTeixeiraC <uchih43@gmail.com>
2023-09-27 13:52:41 +0100

    new

--- game-store/script/js.js
+++ game-store/script/js.js
@@ -4,4 +4,36 @@ let navbar = document.querySelector('.menu')
 menu.onclick = () => {
     navbar.classList.toggle('active')
     menu.classList.toggle('move')
+    bell.classList.remove('active')
 }
+// notification 
+
+let bell = document.querySelector('.notification')
+
+document.querySelector('#bell-icon').onclick = () => {
+    bell.classList.toggle('active');
+}
+
+// Swipper
+var swiper = new Swiper(".trending-content", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+      1068: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+    },
+  });
